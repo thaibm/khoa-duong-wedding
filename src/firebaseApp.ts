74c1707c8bde27d8becfa29fb5ddf2135049b90d
Bùi Minh Thái <thai.buiminh@ncc.asia>
2024-07-19 09:23:58 +0700

    add github action auto deploy

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -7,7 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBTLC3MB-MnF9zfrd3SupJ1RYESfQghamg",
+  authDomain: "khoa-duong.firebaseapp.com",
+  databaseURL: "https://khoa-duong-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "khoa-duong",
+  storageBucket: "khoa-duong.appspot.com",
+  messagingSenderId: "467242392926",
+  appId: "1:467242392926:web:4b7845baddfbd5e921c3cf",
+  measurementId: "G-QG0WJ2NHYQ"
 };
 
 // Initialize Firebase
